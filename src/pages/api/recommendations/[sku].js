9b6c9f7f6135ce7c01794f65e84f0d73bf9d7963
Håkon Krogh <hakonhk@gmail.com>
2021-04-15 11:24:12 +0200

    Cleanup in api fn

--- src/pages/api/recommendations/[sku].js
+++ src/pages/api/recommendations/[sku].js
@@ -1,6 +1,5 @@
 export default async (req, res) => {
   const response = await fetch(
-    // `https://retail.googleapis.com/v2/projects/${process.env.NEXT_PUBLIC_GRE_PROJECTID}/locations/global/catalogs/default_catalog/placements/recently_viewed_default:predict?token=${process.env.NEXT_PUBLIC_GRE_APIKEY}`,
     `https://recommendationengine.clients6.google.com/v1beta1/projects/${process.env.GRE_PROJECTNUMBER}/locations/global/catalogs/default_catalog/eventStores/default_event_store/placements/recently_viewed_default:predict?key=${process.env.NEXT_PUBLIC_GRE_APIKEY}`,
     {
       method: 'post',
@@ -32,11 +31,9 @@ export default async (req, res) => {
 
   if (!response.ok) {
     res.json(await response.json());
-    return;
     // throw new Error('Could not get recommendations');
+    return;
   }
 
   res.json(await response.json());
-
-  // res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
 };
